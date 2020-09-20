480884a5dc05f643c585549451e70fe243e1087a
João Carlos Ferra de Almeida <jcfd.almeida@campus.fct.unl.pt>
2020-09-20 21:48:20 +0100

    fd

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ sr.reveal('.skills__data',{interval: 200});
 sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.timeline',{interval: 200}); 
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
